27b5cc9d357972c02c469ea0f1cd52d5e5854e28
inho-park <ttotto0608@naver.com>
2022-10-19 01:51:30 +0900

    add some Remark

--- springLegacy/src/main/java/org/zerock/sample/Chef.java
+++ springLegacy/src/main/java/org/zerock/sample/Chef.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
+// @Component 는 @ContextConfiguration 에 해당되는 root_context.xml 의 context:component-scan, 
+// 혹은 java class 에서  @Configuration 을 통해 명시해주는 @ComponentScan() 을 통해 생성할 Bean 명시
 @Component
 @Data
 public class Chef {
--- springLegacy/src/main/java/org/zerock/sample/Restaurant.java
+++ springLegacy/src/main/java/org/zerock/sample/Restaurant.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 @Data
 public class Restaurant {
 	
+    // Restaurant 객체에 Chef 가 객체가 필요하다는 @Autowired 를 설정하여
+    // Spring 이 Chef 객체의 reference => bean 을 주입
 	@Setter (onMethod_ =  @Autowired )
 	private Chef chef;
 }
