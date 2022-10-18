b4afe609b4d2ffd00c1e9dfc59cfe93fff73c2b9
inho-park <ttotto0608@naver.com>
2022-10-19 01:34:09 +0900

    setting JDBC

--- springLegacy/src/main/java/org/zerock/config/RootConfig.java
+++ springLegacy/src/main/java/org/zerock/config/RootConfig.java
@@ -3,8 +3,8 @@ package org.zerock.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages= {"org.zerock.sample"})
+//@Configuration
+//@ComponentScan(basePackages= {"org.zerock.sample"})
 public class RootConfig {
 
 }
