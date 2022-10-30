2a3c7787d259c37931ea6c5824414a960b139ac0
inho-park <ttotto0608@naver.com>
2022-10-31 01:37:18 +0900

    access 404Error

--- jex01/src/main/java/com/zerock/config/WebConfig.java
+++ jex01/src/main/java/com/zerock/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.zerock.config;
 
+import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -21,4 +24,12 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 		// TODO Auto-generated method stub
 		return new String[] {"/"};
 	}
+
+	@Override
+	protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+		// TODO Auto-generated method stub
+		registration.setInitParameter("throwExceptionIfNoFound", "true");
+	}
+	
+	
 }
