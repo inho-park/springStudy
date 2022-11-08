84fe73b7594e45afa37c888c8a7b810b3f4f14d0
inho-park <ttotto0608@naver.com>
2022-11-08 23:28:01 +0900

    Failed to instantiate SLF4J LoggerFactory...sibal

--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -83,30 +83,30 @@ public class BoardControllerTests {
 			.getModelMap());
 	}
 	
-	@Test
-	public void testModify() throws Exception {
-		String resultPage = mockMvc
-							.perform(MockMvcRequestBuilders.post("/board/modify")
-							.param("bno", "1")
-							.param("title", "수정된 테스트 새글 제목")
-							.param("content", "수정된 테스트 새글 내용")
-							.param("writer","수정된 새로운 작가래유"))
-						.andReturn()
-						.getModelAndView()
-						.getViewName();
-		
-		log.info(resultPage);
-	}
-	
-	@Test
-	public void testRemove() throws Exception {
-		String resultPage = mockMvc
-							.perform(MockMvcRequestBuilders.post("/board/remove")
-							.param("bno", "21"))
-						.andReturn()
-						.getModelAndView()
-						.getViewName();
-		
-		log.info(resultPage);
-	}
+//	@Test
+//	public void testModify() throws Exception {
+//		String resultPage = mockMvc
+//							.perform(MockMvcRequestBuilders.post("/board/modify")
+//							.param("bno", "1")
+//							.param("title", "수정된 테스트 새글 제목")
+//							.param("content", "수정된 테스트 새글 내용")
+//							.param("writer","수정된 새로운 작가래유"))
+//						.andReturn()
+//						.getModelAndView()
+//						.getViewName();
+//		
+//		log.info(resultPage);
+//	}
+//	
+//	@Test
+//	public void testRemove() throws Exception {
+//		String resultPage = mockMvc
+//							.perform(MockMvcRequestBuilders.post("/board/remove")
+//							.param("bno", "21"))
+//						.andReturn()
+//						.getModelAndView()
+//						.getViewName();
+//		
+//		log.info(resultPage);
+//	}
 }
