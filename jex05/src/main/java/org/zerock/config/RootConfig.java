140fdcbe3e0309f0818b5d4e5be5383d6f7aa6f3
inho-park <ttotto0608@naver.com>
2022-12-22 00:51:43 +0900

    multipart Resolving

---
+++ jex05/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,38 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.mybatis.spring.SqlSessionFactoryBean;
+//import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+//@ComponentScan(basePackages="org.zerock.service")
+//@MapperScan(basePackages= {"org.zerock.mapper"})
+public class RootConfig {
+	
+//	@Bean
+//	public DataSource dataSource() {
+//		HikariConfig config = new HikariConfig();
+//		config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+//		config.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1522:XE");
+//		config.setUsername("scott");
+//		config.setPassword("tiger");
+//		HikariDataSource dataSource = new HikariDataSource(config);
+//		
+//		return dataSource;
+//	}
+//	
+//	@Bean
+//	public SqlSessionFactory sqlSessionFactory() throws Exception {
+//		SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+//		factory.setDataSource(dataSource());
+//		return (SqlSessionFactory)factory.getObject();
+//	}
+}
