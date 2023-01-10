3dd4fe3aaec47c60b45fadd7f5c7a7c3d09ca637
inho-park <ttotto0608@naver.com>
2023-01-10 17:14:03 +0900

    security with spring default security form

--- ex06/src/main/java/org/zerock/controller/CommonController.java
+++ ex06/src/main/java/org/zerock/controller/CommonController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.extern.log4j.Log4j;
 
@@ -17,4 +18,30 @@ public class CommonController {
 		model.addAttribute("msg","Access Denied");
 	}
 	
+	
+	@GetMapping("/customLogin")
+	public void loginInput(String error, String logout, Model model) {
+		log.info("error : " + error);
+		log.info("logout : " + logout);
+		
+		if(error != null) {
+			model.addAttribute("error", "Login Error Check Your Account");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("logout", "Logout Attribute");
+		}
+	}
+	
+	
+	@GetMapping("/customLogout")
+	public void logoutGET() {
+		log.info("custom logout");
+	}
+	
+	
+	@PostMapping("/customLogout")
+	public void logoutPost() {
+		log.info("post custom logout");
+	}
 }
