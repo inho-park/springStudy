a23599c70e8ac716b4d13d2d46a8da3809bbc698
inho-park <ttotto0608@naver.com>
2023-01-15 17:45:14 +0900

    test passwordEncoder with java

--- jex06/src/main/java/org/zerock/config/SecurityConfig.java
+++ jex06/src/main/java/org/zerock/config/SecurityConfig.java
@@ -1,9 +1,15 @@
 package org.zerock.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.zerock.security.CustomLoginSuccessHandler;
 
 import lombok.extern.log4j.Log4j;
 
@@ -12,12 +18,51 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		log.info("configure.....................................................");
+		
+		auth.inMemoryAuthentication()
+			.withUser("admin")
+			.password("{noop}admin")
+			.roles("ADMIN");
+	
+		auth.inMemoryAuthentication()
+			.withUser("member")
+			.password("{noop}member")
+			.roles("MEMBER");
+	}
+	
+	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		
 		http.authorizeRequests()
-			.antMatchers("/sample/all").permitAll()
-			.antMatchers("/sample/member").access("hasRole('ROLE_MEMBER')")
-			.antMatchers("/sample/admin").access("hasRole('ROLE_ADMIN')");
+		.antMatchers("/sample/all").permitAll()
+		.antMatchers("/sample/member").access("hasRole('ROLE_MEMBER')")
+		.antMatchers("/sample/admin").access("hasRole('ROLE_ADMIN')");
+		
+		http.formLogin()
+		.loginPage("/customLogin")
+		.loginProcessingUrl("/login")
+		.successHandler(loginSuccessHandler());
+		
+		http.logout()
+		.logoutUrl("/customLogout")
+		.invalidateHttpSession(true)
+		.deleteCookies("remember-me","JSESSION_ID");
+	}
+	
+	@Bean
+	public AuthenticationSuccessHandler loginSuccessHandler() {
+		return new CustomLoginSuccessHandler();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
